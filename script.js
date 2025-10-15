1c34f5278b420c2ee1084ea2c06528340d2de8f2
Eiziv <gustaf@granstrm.com>
2025-10-16 00:57:28 +0200

    beta

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const container = document.querySelector("#container");
 
-function createCounter(labelText) {
+function createCounter(labelText, counterSetText) {
 	const counter = document.createElement("div");
 	const label = document.createElement("span");
 	const sub = document.createElement("button");
 	const count = document.createElement("span");
 	const add = document.createElement("button");
-	
-	let value = 0;
+
+	const storageKey = `${counterSetText}/${labelText}`;
+	let value = window.localStorage.getItem(storageKey);
+	if (value === null) {
+		value = 0;
+		window.localStorage.setItem(storageKey, value);
+	} else {
+		value = parseInt(value)
+	}
 	
 	function updateCounter(n) {
 		value = value + n;
 		count.textContent = value;
+		window.localStorage.setItem(storageKey, value);
 	}
 	
 	label.textContent = labelText;
@@ -27,6 +35,9 @@ function createCounter(labelText) {
 	counter.appendChild(add);
 	
 	counter.classList.add("counter");
+	label.classList.add("label");
+	count.classList.add("count");
+	
 
 	return counter;
 }
@@ -37,12 +48,11 @@ function addCounterSet(headerText, extraCounters) {
 
 	header.textContent = headerText;
 	counterSet.appendChild(header);
-	counterSet.appendChild(createCounter("Ringt:"));
-	counterSet.appendChild(createCounter("Svart:"));
-	counterSet.appendChild(createCounter("Solgt:"));
-
-	for (const counterText of extraCounters) {
-		counterSet.appendChild(createCounter(counterText));
+	
+	const counters = ["Ringt:", "Svart:", "Solgt:"].concat(extraCounters);
+	
+	for (const counterText of counters) {
+		counterSet.appendChild(createCounter(counterText, headerText));
 	}
 	
 	counterSet.classList.add("counter-set");
